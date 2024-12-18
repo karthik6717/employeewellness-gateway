085cdbb074e373084fbd08b6df9bee8716400b38
G KARTHIK <gollak64@gmail.com>
2024-12-18 22:44:58 +0530

    port numbers are updated

---
+++ src/test/java/employeewellness_gateway/EmployeewellnessGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package employeewellness_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeewellnessGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
